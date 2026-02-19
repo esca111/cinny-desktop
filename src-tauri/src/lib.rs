a864de65ec54d87f9ecffd668f4122ff0f52a1dd
esca111 <jennyw111@proton.me>
2026-02-19 13:26:14 +1300

    fix notifications on local compile

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -5,11 +5,7 @@
 
 // mod menu;
 
-use tauri::{webview::WebviewWindowBuilder, WebviewUrl};
-
 pub fn run() {
-    let port: u16 = 44548;
-    let context = tauri::generate_context!();
     let builder = tauri::Builder::default();
 
     // #[cfg(target_os = "macos")]
@@ -18,17 +14,8 @@ pub fn run() {
     // }
 
     builder
-        .plugin(tauri_plugin_localhost::Builder::new(port).build())
-        .plugin(tauri_plugin_window_state::Builder::default().build())
         .plugin(tauri_plugin_notification::init())
-        .setup(move |app| {
-            let url = format!("http://localhost:{}", port).parse().unwrap();
-            let window_url = WebviewUrl::External(url);
-            WebviewWindowBuilder::new(app, "main".to_string(), window_url)
-                .title("Cinny")
-                .build()?;
-            Ok(())
-        })
-        .run(context)
+        .plugin(tauri_plugin_window_state::Builder::default().build())
+        .run(tauri::generate_context!())
         .expect("error while building tauri application");
 }
